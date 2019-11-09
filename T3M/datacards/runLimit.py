4b093ef1f617a176da4c007d43b662af4572a972
Vladimir <vladimir.cherepanov@cern.ch>
2019-11-09 18:01:00 +0100

    put blind limit calculation

--- T3M/datacards/runLimit.py
+++ T3M/datacards/runLimit.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     parser.add_argument("-i", "--input-file",help="input file. [Default: %(default)s] ", action="store", default = 'CMS_T3MSignal_13TeV_Combined.txt')
     args = parser.parse_args()
     command ="combineTool.py -M T2W -o workspace.root -i "+args.input_file+";"
-    command +="combineTool.py -M AsymptoticLimits -d workspace.root;"
+    command +="combineTool.py -M AsymptoticLimits  --run blind  -d workspace.root;"
 
 
     print command
