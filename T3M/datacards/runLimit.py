56b7174cc7fee27d554867b8c5b39f5962d06c71
Federica Simone <39649267+fsimone91@users.noreply.github.com>
2020-01-31 16:51:37 +0100

    Update runLimit.py

--- T3M/datacards/runLimit.py
+++ T3M/datacards/runLimit.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     parser.add_argument("-i", "--input-file",help="input file. [Default: %(default)s] ", action="store", default = 'CMS_T3MSignal_13TeV_Combined.txt')
     args = parser.parse_args()
     command ="combineTool.py -M T2W -o workspace.root -i "+args.input_file+";"
-    command +="combineTool.py -M AsymptoticLimits  --run blind  -d workspace.root;"
+    command +="combineTool.py -M AsymptoticLimits  --run blind  -d workspace.root --cl 0.90 -m 1.777;"
 
 
     print command
