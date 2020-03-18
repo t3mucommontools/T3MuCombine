eaf37cd18f6bafc7cec3702d33713978060eb175
Vladimir <vladimir.cherepanov@cern.ch>
2020-03-18 18:57:31 +0100

    upd manual card producer

--- T3MLimit/produce_cards_manual_points.sh
+++ T3MLimit/produce_cards_manual_points.sh
@@ -9,11 +9,11 @@ bdtC1='0.165342'
 bdtC2='0.0566122'
 
 
-./makeTheCard.py --selection='bdt >'$bdtA1'& category==1 '                  --category='Unblind2016A1'$wp --unblinded  --datafile T3MMiniTree2016Vars.root
-./makeTheCard.py --selection='bdt >'$bdtA2'&bdt<'$bdtA1'& category==1 '     --category='Unblind2016A2'$wp --unblinded  --datafile T3MMiniTree2016Vars.root
-./makeTheCard.py --selection='bdt >'$bdtB1'& category==2 '                  --category='Unblind2016B1'$wp --unblinded  --datafile T3MMiniTree2016Vars.root
-./makeTheCard.py --selection='bdt >'$bdtB2'&bdt<'$bdtB1'& category==2 '     --category='Unblind2016B2'$wp --unblinded  --datafile T3MMiniTree2016Vars.root
-./makeTheCard.py --selection='bdt >'$bdtC1'& category==3 '                  --category='Unblind2016C1'$wp --unblinded  --datafile T3MMiniTree2016Vars.root
-./makeTheCard.py --selection='bdt >'$bdtC2'&bdt<'$bdtC1'& category==3 '     --category='Unblind2016C2'$wp --unblinded  --datafile T3MMiniTree2016Vars.root
+./makeTheCard.py --selection='bdt >'$bdtA1'& category==1 '                  --category='2016A1'$wp   --datafile T3MMiniTree2016Vars.root
+./makeTheCard.py --selection='bdt >'$bdtA2'&bdt<'$bdtA1'& category==1 '     --category='2016A2'$wp   --datafile T3MMiniTree2016Vars.root
+./makeTheCard.py --selection='bdt >'$bdtB1'& category==2 '                  --category='2016B1'$wp   --datafile T3MMiniTree2016Vars.root
+./makeTheCard.py --selection='bdt >'$bdtB2'&bdt<'$bdtB1'& category==2 '     --category='2016B2'$wp   --datafile T3MMiniTree2016Vars.root
+./makeTheCard.py --selection='bdt >'$bdtC1'& category==3 '                  --category='2016C1'$wp   --datafile T3MMiniTree2016Vars.root
+./makeTheCard.py --selection='bdt >'$bdtC2'&bdt<'$bdtC1'& category==3 '     --category='2016C2'$wp   --datafile T3MMiniTree2016Vars.root
 
 
