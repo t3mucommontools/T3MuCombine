a7e9e000ed78dd9db9df3d2cbe299e40b1848d04
bjoshi <bhargav.madhusudan.joshi@cern.ch>
2021-06-03 11:29:36 +0200

    bigfixes

--- T3M/run.py
+++ T3M/run.py
@@ -11,10 +11,26 @@ import array
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input-file",help="input file. [Default: %(default)s] ", action="store", default = 'input_histograms.root')
+    parser.add_argument("-c", "--card-file",help="model card file. [Default: %(default)s] ", action="store", default = 'model_card.rs')
+    parser.add_argument("-t", "--type", help="specify type (threeGlobal/twoGlobalTracker)", action="store", default="threeGlobal")
+    parser.add_argument("-r", "--run", help="Run (2017/2018)", action="store", default="2018")
+    parser.add_argument("-d", "--dimensions",help="Type of binning used (1 for 1D, 2 for 2D)",type=int, action="store",default=1)
     args = parser.parse_args()
 
-    command="root -b -q 'createDataCards_v2.cxx(\" " + args.input_file+ "\" )';"
-    command +="cd datacards; combineCards.py CMS_T3MSignal_13TeV_A1.txt CMS_T3MSignal_13TeV_A2.txt CMS_T3MSignal_13TeV_B1.txt CMS_T3MSignal_13TeV_B2.txt  CMS_T3MSignal_13TeV_C1.txt CMS_T3MSignal_13TeV_C2.txt > CMS_T3MSignal_13TeV_Combined.txt;"
+    command = ""
+    print(args.dimensions)
+
+    if (args.dimensions==1): 
+        command="root -b -q 'createDataCards_v2.cxx(\"" + args.input_file+ "\", 0, false, \""+ args.card_file +"\",\""+args.type+"\",\""+args.run+"\")';"
+        command +="cd datacards; combineCards.py CMS_T3MSignal_13TeV_A1.txt CMS_T3MSignal_13TeV_A2.txt CMS_T3MSignal_13TeV_B1.txt CMS_T3MSignal_13TeV_B2.txt"
+        command +="  CMS_T3MSignal_13TeV_C1.txt CMS_T3MSignal_13TeV_C2.txt > CMS_T3MSignal_13TeV_Combined.txt;"
+    elif (args.dimensions==2):
+        command="root -b -q 'create2DDataCards.cxx(\"" + args.input_file+ "\", 0, false, \""+ args.card_file +"\",\""+args.type+"\",\""+args.run+"\")';"
+        command +="cd datacards; combineCards.py CMS_T3MSignal_13TeV_A11.txt CMS_T3MSignal_13TeV_A12.txt CMS_T3MSignal_13TeV_A21.txt CMS_T3MSignal_13TeV_A22.txt "
+        command +="CMS_T3MSignal_13TeV_B11.txt CMS_T3MSignal_13TeV_B12.txt CMS_T3MSignal_13TeV_B21.txt CMS_T3MSignal_13TeV_B22.txt "
+        command +="CMS_T3MSignal_13TeV_C11.txt CMS_T3MSignal_13TeV_C12.txt CMS_T3MSignal_13TeV_C21.txt CMS_T3MSignal_13TeV_C22.txt > CMS_T3MSignal_13TeV_Combined.txt;"
+    else: 
+        print("Invalid number of dimensions!")
 
 
     print command
