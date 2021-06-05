3479a6b34045c18173c3ad6c4a48a667026937bd
bmjoshi <joshi.bm163@gmail.com>
2021-06-05 16:36:17 -0400

    updated run.py
    Bugfix A2 -> A3

--- T3M/run.py
+++ T3M/run.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
            command +="  CMS_T3MSignal_13TeV_C1.txt CMS_T3MSignal_13TeV_C2.txt > CMS_T3MSignal_13TeV_Combined.txt;"
         elif args.version==3:
            command="root -b -q 'createDataCards_v3.cxx(\"" + args.input_file+ "\", 0, false, \""+ args.card_file +"\",\""+args.type+"\",\""+args.run+"\")';"
-           command +="cd datacards; combineCards.py CMS_T3MSignal_13TeV_A1.txt CMS_T3MSignal_13TeV_A2.txt CMS_T3MSignal_13TeV_A2.txt CMS_T3MSignal_13TeV_B1.txt CMS_T3MSignal_13TeV_B2.txt"
+           command +="cd datacards; combineCards.py CMS_T3MSignal_13TeV_A1.txt CMS_T3MSignal_13TeV_A2.txt CMS_T3MSignal_13TeV_A3.txt CMS_T3MSignal_13TeV_B1.txt CMS_T3MSignal_13TeV_B2.txt"
            command +=" CMS_T3MSignal_13TeV_B3.txt  CMS_T3MSignal_13TeV_C1.txt CMS_T3MSignal_13TeV_C2.txt CMS_T3MSignal_13TeV_C3.txt> CMS_T3MSignal_13TeV_Combined.txt;"
 
     elif (args.dimensions==2):
