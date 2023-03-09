4e59a081a11f3b9dd8bd14b754d05b2b234979e4
fsimone <federica.maria.simone@cern.ch>
2023-03-09 14:54:09 +0100

    corrected signal and background weights for combined mass plotc

--- python/massplots_sbweighted.py
+++ python/massplots_sbweighted.py
@@ -1,4 +1,5 @@
 import ROOT
+import math 
 #ROOT.gROOT.SetBatch(True)
 
 class category:
@@ -25,14 +26,17 @@ class category:
     self.sig = self.ws.obj(sigl)
     self.x = self.rds.get().find('m3m')
     self.x.SetTitle('3#mu mass [GeV]')
-    self.e = e
+    self.e = e*e
     self.ns = s
     self.nb = b
+    self.ams = math.sqrt(2*( (float(s)+float(b)) * math.log( 1.0+float(s)/float(b) ) - float(s)))
   def weight(self, w):
     ''' create a weight of value w and add it to the dataset.
     the dataset is not weighted at this point but contains the info
+    note: signal pdfs are weighted using sqrt(w and the signal yields
     '''
-    self.wgt  = ROOT.RooRealVar("wgt", '', w)
+    self.wgt    = ROOT.RooRealVar("wgt", '', w)
+    self.wgtsig = ROOT.RooRealVar("wgtsig", '', self.ns * math.sqrt(w))
     self.rds.addColumn(self.wgt)
 
 B3GL_2017='ThreeGlobal/2017/unblind/multipdf/workspaces/CMS_T3MBkg_13TeV.root'
@@ -66,23 +70,37 @@ def merge_and_plot(categories):
   '''
   norm = sum(1./c.e for c in categories)
 #  norm = sum(c.ns/c.nb for c in categories)
+#  norm = sum(c.ams for c in categories)
   for cat in categories:
     cat.weight(1./cat.e/norm)
 #    cat.weight(cat.ns/cat.nb/norm)
+#    cat.weight(cat.ams/norm)
   merged = ROOT.RooDataSet('merged', '', categories[0].rds.get())
+
   for i, cat in enumerate(categories):
     merged.append(cat.rds)
   wdata  = ROOT.RooDataSet(merged.GetName(), merged.GetTitle(), merged, merged.get(), "", 'wgt')
+  #wdata.Print("V")
+ 
   sigs = ROOT.RooArgList()
   wgts = ROOT.RooArgList()
   for cat in categories:
     sigs.add(cat.sig)
-    wgts.add(cat.wgt)
+    wgts.add(cat.wgtsig)
+
+  #setting recursiveFractions=ROOT.kTRUE, the coefficients will sum to 1
   sums = ROOT.RooAddPdf('sums', '', sigs, wgts)
-  signorm = sum(c.ns*c.wgt.getValV() for c in categories)
+  signorm =  sum(c.wgtsig.getValV() for c in categories)
+
+  print('signorm ',signorm)
+  print('norm ', norm)
+
   plot  = categories[0].x.frame(40)
+  categories[0].x.setRange("fullRange",1.62,2.0)
+
   wdata.plotOn(plot)
-  sums.plotOn(plot, ROOT.RooFit.Normalization(signorm,ROOT.RooAbsReal.NumEvent), ROOT.RooFit.LineColor(ROOT.kRed))
+  
+  sums.plotOn(plot, ROOT.RooFit.Normalization(signorm,ROOT.RooAbsReal.NumEvent), ROOT.RooFit.Range("fullRange"), ROOT.RooFit.NormRange("fullRange"), ROOT.RooFit.LineColor(ROOT.kRed))
   plot.GetYaxis().SetTitle('Weighted entries / 10 MeV') 
 
   leg = ROOT.TLegend(0.48,0.60,0.86,0.78)
@@ -105,51 +123,51 @@ def merge_and_plot(categories):
 
 categories_expweighted = [
   # 3GL 2017
-  category(name='B3GL_A117', fileb=B3GL_2017, files=S3GL_2017, e=23.0, s=1.04, b=0.79, wspacel='w_all', datal='data_obs_A1', sigl='SignalModel_A1'),
-  category(name='B3GL_B117', fileb=B3GL_2017, files=S3GL_2017, e=21.9, s=1.50, b=2.49, wspacel='w_all', datal='data_obs_B1', sigl='SignalModel_B1'),
-  category(name='B3GL_C117', fileb=B3GL_2017, files=S3GL_2017, e=33.6, s=1.99, b=13.0, wspacel='w_all', datal='data_obs_C1', sigl='SignalModel_C1'),
-  category(name='B3GL_A217', fileb=B3GL_2017, files=S3GL_2017, e=17.0, s=4.27, b=18.9, wspacel='w_all', datal='data_obs_A2', sigl='SignalModel_A2'),
-  category(name='B3GL_B217', fileb=B3GL_2017, files=S3GL_2017, e=20.5, s=2.88, b=12.5, wspacel='w_all', datal='data_obs_B2', sigl='SignalModel_B2'),
-  category(name='B3GL_C217', fileb=B3GL_2017, files=S3GL_2017, e=47.7, s=3.57, b=107 , wspacel='w_all', datal='data_obs_C2', sigl='SignalModel_C2'),
-  category(name='B3GL_A317', fileb=B3GL_2017, files=S3GL_2017, e=22.8, s=7.88, b=135 , wspacel='w_all', datal='data_obs_A3', sigl='SignalModel_A3'),
-  category(name='B3GL_B317', fileb=B3GL_2017, files=S3GL_2017, e=18.8, s=12.5, b=243 , wspacel='w_all', datal='data_obs_B3', sigl='SignalModel_B3'),
-  category(name='B3GL_C317', fileb=B3GL_2017, files=S3GL_2017, e=108., s=2.79, b=343 , wspacel='w_all', datal='data_obs_C3', sigl='SignalModel_C3'),
+  category(name='B3GL_A117', fileb=B3GL_2017, files=S3GL_2017, e=2.30, s=1.0388 , b=1.05926, wspacel='w_all', datal='data_obs_A1', sigl='SignalModel_A1'),
+  category(name='B3GL_B117', fileb=B3GL_2017, files=S3GL_2017, e=2.19, s=1.50467, b=2.52061, wspacel='w_all', datal='data_obs_B1', sigl='SignalModel_B1'),
+  category(name='B3GL_C117', fileb=B3GL_2017, files=S3GL_2017, e=3.36, s=1.9952 , b=12.3963, wspacel='w_all', datal='data_obs_C1', sigl='SignalModel_C1'),
+  category(name='B3GL_A217', fileb=B3GL_2017, files=S3GL_2017, e=1.70, s=4.2737 , b=18.5647, wspacel='w_all', datal='data_obs_A2', sigl='SignalModel_A2'),
+  category(name='B3GL_B217', fileb=B3GL_2017, files=S3GL_2017, e=2.05, s=2.87582, b=12.4856, wspacel='w_all', datal='data_obs_B2', sigl='SignalModel_B2'),
+  category(name='B3GL_C217', fileb=B3GL_2017, files=S3GL_2017, e=4.77, s=3.57411, b=107.263, wspacel='w_all', datal='data_obs_C2', sigl='SignalModel_C2'),
+  category(name='B3GL_A317', fileb=B3GL_2017, files=S3GL_2017, e=2.28, s=7.88048, b=139.2  , wspacel='w_all', datal='data_obs_A3', sigl='SignalModel_A3'),
+  category(name='B3GL_B317', fileb=B3GL_2017, files=S3GL_2017, e=1.88, s=12.4699, b=250.512, wspacel='w_all', datal='data_obs_B3', sigl='SignalModel_B3'),
+  category(name='B3GL_C317', fileb=B3GL_2017, files=S3GL_2017, e=10.8, s=2.78574, b=336.078, wspacel='w_all', datal='data_obs_C3', sigl='SignalModel_C3'),
   # 3GL 2018
-  category(name='B3GL_A118', fileb=B3GL_2018, files=S3GL_2018, e=12.3, s=4.60, b=10.0, wspacel='w_all', datal='data_obs_A1', sigl='SignalModel_A1'),
-  category(name='B3GL_B118', fileb=B3GL_2018, files=S3GL_2018, e=17.2, s=2.42, b=4.94, wspacel='w_all', datal='data_obs_B1', sigl='SignalModel_B1'),
-  category(name='B3GL_C118', fileb=B3GL_2018, files=S3GL_2018, e=17.9, s=2.60, b=5.95, wspacel='w_all', datal='data_obs_C1', sigl='SignalModel_C1'),
-  category(name='B3GL_A218', fileb=B3GL_2018, files=S3GL_2018, e=12.9, s=9.42, b=59.7, wspacel='w_all', datal='data_obs_A2', sigl='SignalModel_A2'),
-  category(name='B3GL_B218', fileb=B3GL_2018, files=S3GL_2018, e=10.0, s=12.8, b=59.4, wspacel='w_all', datal='data_obs_B2', sigl='SignalModel_B2'),
-  category(name='B3GL_C218', fileb=B3GL_2018, files=S3GL_2018, e=21.3, s=4.84, b=37.3, wspacel='w_all', datal='data_obs_C2', sigl='SignalModel_C2'),
-  category(name='B3GL_A318', fileb=B3GL_2018, files=S3GL_2018, e=18.1, s=16.7, b=393 , wspacel='w_all', datal='data_obs_A3', sigl='SignalModel_A3'),
-  category(name='B3GL_B318', fileb=B3GL_2018, files=S3GL_2018, e=13.5, s=33.8, b=944 , wspacel='w_all', datal='data_obs_B3', sigl='SignalModel_B3'),
-  category(name='B3GL_C318', fileb=B3GL_2018, files=S3GL_2018, e=28.4, s=10.7, b=346 , wspacel='w_all', datal='data_obs_C3', sigl='SignalModel_C3'),
+  category(name='B3GL_A118', fileb=B3GL_2018, files=S3GL_2018, e=1.23, s=4.5975, b=10.0293, wspacel='w_all', datal='data_obs_A1', sigl='SignalModel_A1'),
+  category(name='B3GL_B118', fileb=B3GL_2018, files=S3GL_2018, e=1.72, s=2.4183, b=4.93178, wspacel='w_all', datal='data_obs_B1', sigl='SignalModel_B1'),
+  category(name='B3GL_C118', fileb=B3GL_2018, files=S3GL_2018, e=1.79, s=2.6001, b=5.91992, wspacel='w_all', datal='data_obs_C1', sigl='SignalModel_C1'),
+  category(name='B3GL_A218', fileb=B3GL_2018, files=S3GL_2018, e=1.29, s=9.4200, b=59.7067, wspacel='w_all', datal='data_obs_A2', sigl='SignalModel_A2'),
+  category(name='B3GL_B218', fileb=B3GL_2018, files=S3GL_2018, e=1.00, s=12.836, b=70.7446, wspacel='w_all', datal='data_obs_B2', sigl='SignalModel_B2'),
+  category(name='B3GL_C218', fileb=B3GL_2018, files=S3GL_2018, e=2.13, s=4.8412, b=37.1054, wspacel='w_all', datal='data_obs_C2', sigl='SignalModel_C2'),
+  category(name='B3GL_A318', fileb=B3GL_2018, files=S3GL_2018, e=1.81, s=16.663, b=398.711, wspacel='w_all', datal='data_obs_A3', sigl='SignalModel_A3'),
+  category(name='B3GL_B318', fileb=B3GL_2018, files=S3GL_2018, e=1.35, s=33.761, b=952.281, wspacel='w_all', datal='data_obs_B3', sigl='SignalModel_B3'),
+  category(name='B3GL_C318', fileb=B3GL_2018, files=S3GL_2018, e=2.84, s=10.686, b=355.332, wspacel='w_all', datal='data_obs_C3', sigl='SignalModel_C3'),
   # 2GL 2017
-  category(name='B2GL_A117', fileb=B2GL_2017, files=S2GL_2017, e=48.8, s=0.65, b=2.08, wspacel='w_all', datal='data_obs_A1', sigl='SignalModel_A1'),
-  category(name='B2GL_B117', fileb=B2GL_2017, files=S2GL_2017, e=59.4, s=1.28, b=9.93, wspacel='w_all', datal='data_obs_B1', sigl='SignalModel_B1'),
-  category(name='B2GL_C117', fileb=B2GL_2017, files=S2GL_2017, e=143., s=0.34, b=6.30, wspacel='w_all', datal='data_obs_C1', sigl='SignalModel_C1'),
-  category(name='B2GL_A217', fileb=B2GL_2017, files=S2GL_2017, e=62.2, s=1.36, b=26.7, wspacel='w_all', datal='data_obs_A2', sigl='SignalModel_A2'),
-  category(name='B2GL_B217', fileb=B2GL_2017, files=S2GL_2017, e=72.2, s=3.31, b=224 , wspacel='w_all', datal='data_obs_B2', sigl='SignalModel_B2'),
-  category(name='B2GL_C217', fileb=B2GL_2017, files=S2GL_2017, e=225., s=0.50, b=47.5, wspacel='w_all', datal='data_obs_C2', sigl='SignalModel_C2'),
+  category(name='B2GL_A117', fileb=B2GL_2017, files=S2GL_2017, e=4.88, s=0.6578, b=2.08188, wspacel='w_all', datal='data_obs_A1', sigl='SignalModel_A1'),
+  category(name='B2GL_B117', fileb=B2GL_2017, files=S2GL_2017, e=5.94, s=1.2751, b=12.3156, wspacel='w_all', datal='data_obs_B1', sigl='SignalModel_B1'),
+  category(name='B2GL_C117', fileb=B2GL_2017, files=S2GL_2017, e=1.43, s=0.3390, b=6.77482, wspacel='w_all', datal='data_obs_C1', sigl='SignalModel_C1'),
+  category(name='B2GL_A217', fileb=B2GL_2017, files=S2GL_2017, e=6.22, s=1.3560, b=26.5756, wspacel='w_all', datal='data_obs_A2', sigl='SignalModel_A2'),
+  category(name='B2GL_B217', fileb=B2GL_2017, files=S2GL_2017, e=7.22, s=3.3052, b=244.133, wspacel='w_all', datal='data_obs_B2', sigl='SignalModel_B2'),
+  category(name='B2GL_C217', fileb=B2GL_2017, files=S2GL_2017, e=22.5, s=0.5037, b=47.8639, wspacel='w_all', datal='data_obs_C2', sigl='SignalModel_C2'),
   # 2GL 2018
-  category(name='B2GL_A118', fileb=B2GL_2018, files=S2GL_2018,  e=35.5, s=1.09, b=6.49, wspacel='w_all', datal='data_obs_A1', sigl='SignalModel_A1'),
-  category(name='B2GL_B118', fileb=B2GL_2018, files=S2GL_2018,  e=26.2, s=4.61, b=54.4, wspacel='w_all', datal='data_obs_B1', sigl='SignalModel_B1'),
-  category(name='B2GL_C118', fileb=B2GL_2018, files=S2GL_2018,  e=45.6, s=2.43, b=42.0, wspacel='w_all', datal='data_obs_C1', sigl='SignalModel_C1'),
-  category(name='B2GL_A218', fileb=B2GL_2018, files=S2GL_2018,  e=55.0, s=1.63, b=31.0, wspacel='w_all', datal='data_obs_A2', sigl='SignalModel_A2'),
-  category(name='B2GL_B218', fileb=B2GL_2018, files=S2GL_2018,  e=45.5, s=8.12, b=487 , wspacel='w_all', datal='data_obs_B2', sigl='SignalModel_B2'),
-  category(name='B2GL_C218', fileb=B2GL_2018, files=S2GL_2018,  e=80.0, s=4.12, b=339 , wspacel='w_all', datal='data_obs_C2', sigl='SignalModel_C2'),
+  category(name='B2GL_A118', fileb=B2GL_2018, files=S2GL_2018,  e=3.55, s=1.0893, b=6.49795, wspacel='w_all', datal='data_obs_A1', sigl='SignalModel_A1'),
+  category(name='B2GL_B118', fileb=B2GL_2018, files=S2GL_2018,  e=2.62, s=4.6117, b=54.9399, wspacel='w_all', datal='data_obs_B1', sigl='SignalModel_B1'),
+  category(name='B2GL_C118', fileb=B2GL_2018, files=S2GL_2018,  e=4.56, s=2.4258, b=42.4175, wspacel='w_all', datal='data_obs_C1', sigl='SignalModel_C1'),
+  category(name='B2GL_A218', fileb=B2GL_2018, files=S2GL_2018,  e=5.50, s=1.6259, b=31.0139, wspacel='w_all', datal='data_obs_A2', sigl='SignalModel_A2'),
+  category(name='B2GL_B218', fileb=B2GL_2018, files=S2GL_2018,  e=4.55, s=8.1209, b=496.256, wspacel='w_all', datal='data_obs_B2', sigl='SignalModel_B2'),
+  category(name='B2GL_C218', fileb=B2GL_2018, files=S2GL_2018,  e=8.00, s=4.1222, b=337.312, wspacel='w_all', datal='data_obs_C2', sigl='SignalModel_C2'),
   # W 2017
-  category(name='W_A17', fileb=BW_A17, files=SW_A17, e=22.4, s=0.14, b=1.4, wspacel='t3m_shapes', datal='data_obs', sigl='sig'),
-  category(name='W_B17', fileb=BW_B17, files=SW_B17, e=25.7, s=0.17, b=4.1, wspacel='t3m_shapes', datal='data_obs', sigl='sig'),
-  category(name='W_C17', fileb=BW_C17, files=SW_C17, e=67.6, s=0.06, b=2.0, wspacel='t3m_shapes', datal='data_obs', sigl='sig'),
+  category(name='W_A17', fileb=BW_A17, files=SW_A17, e=2.24, s=1.4, b=1.4, wspacel='t3m_shapes', datal='data_obs', sigl='sig'),
+  category(name='W_B17', fileb=BW_B17, files=SW_B17, e=2.57, s=1.7, b=4.1, wspacel='t3m_shapes', datal='data_obs', sigl='sig'),
+  category(name='W_C17', fileb=BW_C17, files=SW_C17, e=6.76, s=0.6, b=2.0, wspacel='t3m_shapes', datal='data_obs', sigl='sig'),
   # W 2018
-  category(name='W_A18', fileb=BW_A17, files=SW_A17, e=12.0, s=0.26, b=1.6, wspacel='t3m_shapes', datal='data_obs', sigl='sig'),
-  category(name='W_B18', fileb=BW_B17, files=SW_B17, e=16.6, s=0.16, b=0.7, wspacel='t3m_shapes', datal='data_obs', sigl='sig'),
-  category(name='W_C18', fileb=BW_C17, files=SW_C17, e=46.4, s=0.11, b=4.4, wspacel='t3m_shapes', datal='data_obs', sigl='sig'),
+  category(name='W_A18', fileb=BW_A17, files=SW_A17, e=1.20, s=2.6, b=1.6, wspacel='t3m_shapes', datal='data_obs', sigl='sig'),
+  category(name='W_B18', fileb=BW_B17, files=SW_B17, e=1.66, s=1.6, b=0.7, wspacel='t3m_shapes', datal='data_obs', sigl='sig'),
+  category(name='W_C18', fileb=BW_C17, files=SW_C17, e=4.64, s=1.1, b=4.4, wspacel='t3m_shapes', datal='data_obs', sigl='sig'),
 ]
 can, leg = merge_and_plot(categories_expweighted)
 for cat in categories_expweighted:
-  print('{}\t{}'.format(cat.n, round(cat.wgt.getValV(),4)))
+  print('{}\t{}\t{}\t{}'.format(cat.n, round(cat.wgt.getValV(),4), round(cat.e,4), round(cat.ams,4)))
 
 
 lumitext="2017+2018, 97.8 fb^{-1} (13 TeV)"
@@ -187,7 +205,8 @@ latex.SetTextSize(cmstextsize*tt*0.8)
 
 latex.SetTextFont(extratextfont)
 latex.SetTextSize(cmstextsize*tt)
-#  latex.DrawLatex(posx, posy-1.2*cmstextsize*tt, "Preliminary")
-title = "weighted_massplot.pdf".format(category, '17' if '2017' in lumitext else '18')
+#latex.DrawLatex(posx, posy-1.2*cmstextsize*tt, "Preliminary")
+title = "weighted_massplot".format(category, '17' if '2017' in lumitext else '18')
 leg.Draw()
-can.SaveAs(title, "pdf")
+can.SaveAs(title+".pdf", "pdf")
+can.SaveAs(title+".root", "root")
